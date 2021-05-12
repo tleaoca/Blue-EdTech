cb473319c4c8ea44992226856f4c10252fa63e3d
Thiago <tleaoca@gmail.com>
2021-05-12 20:01:45 -0300

    atualizacao exercicio

--- terminal_unico.py
+++ terminal_unico.py
@@ -1,3 +1,13 @@
+#PROJETO: Gastos com viagem -  Escrever uma aplicação utilizando funções que calcule os gastos com passagem,
+#hospedagem, aluguel de carro e gastos extras de uma viagem para uma determinada cidade.
+
+#Hospedagem - Crie uma função chamada 'custo_hotel' que receba um parâmetro (argumento) chamado 'noites'
+#e retorne o custo total do hotel, sendo que 1 noite custa R$ 140,00.
+
+def custoHotel(noites,valorNoite=140):
+    custoTotal = noites*valorNoite
+    return custoTotal
+
 def custoAviao(nome):
     if nome == 'São Paulo':
         valorPassagem = 312
@@ -9,5 +19,19 @@ def custoAviao(nome):
         valorPassagem = 986
     return valorPassagem
 
-valor = custoAviao('Manaus')
-print(valor)
+def custoCarro(dias,valorDiaria=40):
+    custo = dias*valorDiaria
+    if dias >= 7:
+        custoFinal = custo - 50
+    elif dias >= 3:
+        custoFinal = custo - 20
+    else:
+        custoFinal = custo
+    return custoFinal
+
+def cidadeEscolhida(nomeCidade,diasViagem):
+    custoViagemTotal = custoHotel(diasViagem) + custoAviao(nomeCidade) + custoCarro(diasViagem)
+    return custoViagemTotal
+
+custoGeral = cidadeEscolhida('Manaus',4)
+print(custoGeral)
