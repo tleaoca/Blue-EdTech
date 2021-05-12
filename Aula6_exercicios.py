44f581979ffcfc1757a481585db39a7662a0f53f
Thiago <tleaoca@gmail.com>
2021-05-12 17:54:24 -0300

    atualizacao exercicio 6

--- Aula6_exercicios.py
+++ Aula6_exercicios.py
@@ -90,3 +90,23 @@ comparar(5,3)
 ##DESAFIO -  Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma 
 #string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
 #Considere que Fevereiro tem 28 dias e que a cada 4 anos temos ano bisexto, sendo que nesses casos Fevereiro terá 29 dias.
+
+dia = int(input('Dia: '))
+mes = int(input('Mes: '))
+ano = int(input('Ano: '))
+
+def diaExtenso(dia):
+    listaDia = ["Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Catorzer", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte", "Vinte e Um", "Vinte e Dois", "Vinte e Três", "Vinte e Quatro", "Vinte e Cinco", "Vinte e Seis", "Vinte e Sete", "Vinte e Oito", "Vinte e Nove", "Trinta", "Trinta e Um"]
+    diaEx = listaDia[dia-1]
+    return diaEx
+
+def mesExtenso(mes):
+    listaMes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    mesEx = listaMes[mes-1]
+    return mesEx
+
+print(f'{diaExtenso(dia)} de {mesExtenso(mes)} de {ano}')
+
+
+
+
