0f778eaf3c1fe1a35a1246af46b111e4e08b581d
Thiago Leao <tleaoca@gmail.com>
2021-08-23 13:44:27 -0300

    aula 4

--- MOD 4/Consultorio/Startup.cs
+++ MOD 4/Consultorio/Startup.cs
@@ -45,6 +45,11 @@ namespace Consultorio
             services.AddTransient<IPacienteService, PacienteSqlService>();
             services.AddTransient<IConsultaService, ConsultaSqlService>();
             services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ConsultorioContext>();
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+                {
+                    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- MOD 4/Consultorio/obj/Debug/net5.0/Consultorio.AssemblyInfo.cs
+++ MOD 4/Consultorio/obj/Debug/net5.0/Consultorio.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d8cf4c49-e5de-4306-9189-f03620080e25")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
